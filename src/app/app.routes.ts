99347115bab15ddd0380eabd9e0b5def97b99b9f
garthdp <garthdp4@gmail.com>
2026-04-16 16:06:23 +0200

    chore: commit remaining scaffolding changes from initial setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Gw2 } from './gw2/gw2';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'gw2', component: Gw2 }
+];
